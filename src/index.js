db21c6bb5b523a851c62cec1ce37fa132f2ad68f
Matthew Rubino <mrubino@godaddy.com>
2019-09-05 08:58:33 -0400

    update index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
-const Trademark = require('./trademark');
+const Util = require('./util');
 
-async function get(address) {
-  return await Trademark.getTrademark(address);
+const arguments = process.argv.slice(2);
+if (arguments.length < 1) {
+  usage();
+} else if (arguments[0] === 'trademark') {
+  if (arguments.length !== 2 || !Util.isAddress(arguments[1])) {
+    usage();
+  }
+  getTrademark(arguments[1])
+    .then(results => {
+      console.log(JSON.stringify(results, null, 2));
+      process.exit(0);
+    })
+    .catch(err => {
+      console.error(`Error: ${err.message}`);
+      process.exit(1);
+    });
+} else if (arguments[0] === 'sha') {
+  if (arguments.length !== 2) {
+    usage();
+  }
+  // TODO: get URL and generate SHA of data
+  console.log('TODO');
+} else {
+  usage();
 }
 
-get('0x2022e7859281b85ab6e26e46bdd188830ac31fd1')
-  .then(results => {
-    console.log(JSON.stringify(results, null, 2));
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(`Error: ${err.message}`);
-    process.exit(1);
-  });
+function usage() {
+  console.log('Usage: node src/index.js trademark <address>');
+  console.log('Usage: node src/index.js sha <url>');
+  process.exit(1);
+}
+
+async function getTrademark(address) {
+  const Trademark = require('./trademark');
+  return await Trademark.getTrademark(address);
+}
