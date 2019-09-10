188db4304c91ba6ff649d376a4f1b6b73ecf43fb
dock <chris.murdock8648@gmail.com>
2019-09-10 18:37:42 -0400

    WIP: adding sha generator tool

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import './listings.scss';
-import 'jquery-typeahead';
-import 'jquery-typeahead/dist/jquery.typeahead.min.css';
+import "./listings.scss";
+import "jquery-typeahead";
+import "jquery-typeahead/dist/jquery.typeahead.min.css";
+
+import { resolve } from "url";
 
 // es-lint-ignore
 const $ = require('jquery');
@@ -9,8 +11,37 @@ const BuildTimeline = require('./BuildTimeline');
 const trademarkMap = require('./trademarkMap');
 const templateService = require('./SimpleTemplateService');
 const Handlebars = require('handlebars');
+const crypto = require('crypto');
+
+window.Dropzone = require('./dropzone');
+
+Dropzone.autoDiscover = false;
 
 $().ready(function() {
+  new Dropzone('#dropzone-sha', {
+    dictDefaultMessage: 'Drop image here to generate SHA-256 hash',
+    url: '/',
+    autoQueue: false,
+    addRemoveLinks: true,
+    maxFiles: 1,
+    addedfile: async file => {
+      const hash = crypto.createHash('sha256');
+      const reader = new FileReader();
+      const fileData = await new Promise((resolve, reject) => {
+        reader.onloadend = () => {
+          resolve(reader.result);
+        };
+        reader.onerror = error => {
+          reject(error);
+        };
+        reader.readAsBinaryString(file);
+      });
+      // console.log(fileData);
+      hash.update(fileData, 'utf8');
+      $('#sha-result').html(hash.digest('hex'));
+    },
+  });
+
   $('.js-typeahead').typeahead({
     order: 'asc',
     source: {
@@ -19,18 +50,58 @@ $().ready(function() {
         data: Object.keys(trademarkMap),
       },
     },
+    template: (query, item) => {
+      if (item.display.startsWith('http')) {
+        return `<img src="${item.display}" class="typeahead-thumbnail"/>`;
+      }
+      return '<span>{{display}}</span>';
+    },
+    callback: {
+      onClickAfter: async function() {
+        $('#timeline-content').html(templateService.getTemplate('loading'));
+        const mark = $('#trademark').val();
+        const address = trademarkMap[mark][0];
+        const results = await Trademark.getTrademarkForAddress(address);
+        if (mark.startsWith('http')) {
+          $('#timeline-content').html(
+            templateService.getTemplate('timelineHeader', {
+              address,
+              mark: results.design,
+              markHtml: new Handlebars.SafeString(
+                `<img class="mark-img" src="${mark}"/><div class="design-hash">${results.design}</div>`,
+              ),
+              results: JSON.stringify(results),
+            }),
+          );
+        } else {
+          $('#timeline-content').html(
+            templateService.getTemplate('timelineHeader', {
+              address,
+              mark: results.word,
+              markHtml: new Handlebars.SafeString(`<h2 class="mark-title">${results.word}</h2>`),
+              results: JSON.stringify(results),
+            }),
+          );
+        }
+        if (results.timeline && results.timeline.documents) {
+          BuildTimeline.buildTimeline($('#timeline-content'), results.timeline.documents);
+        }
+      },
+    },
   });
-  $('#submit-address').on('click', async function() {
+  /*$('#submit-address').on('click', async function() {
     $('#timeline-content').html(templateService.getTemplate('loading'));
     const mark = $('#trademark').val();
     const address = trademarkMap[mark][0];
     const results = await Trademark.getTrademarkForAddress(address);
-    if (mark.indexOf('http') !== -1) {
+    if (mark.startsWith('http')) {
       $('#timeline-content').html(
         templateService.getTemplate('timelineHeader', {
           address,
           mark: results.design,
-          markHtml: new Handlebars.SafeString(`<img class="mark-img" src="${mark}"/><div>${results.design}</div>`),
+          markHtml: new Handlebars.SafeString(
+            `<img class="mark-img" src="${mark}"/><div class="design-hash">${results.design}</div>`,
+          ),
           results: JSON.stringify(results),
         }),
       );
@@ -47,5 +118,5 @@ $().ready(function() {
     if (results.timeline && results.timeline.documents) {
       BuildTimeline.buildTimeline($('#timeline-content'), results.timeline.documents);
     }
-  });
+  });*/
 });
